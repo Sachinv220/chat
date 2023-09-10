cef603721059fe7c38658805c54f22cbd738348f
Sachin <vsachin220@gmail.com>
2023-09-10 13:10:21 +0530

    remove ``@db.Char(1)`` redirect user after login

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,6 +1,10 @@
 import SigninButton from "@/components/SigninButton";
+import { getAuthSession } from "@/lib/nextauth";
+import { redirect } from "next/navigation";
 
-const SignIn = () => {
+const SignIn = async () => {
+  const session = await getAuthSession();
+  if (session?.user) redirect("/")
   return (
     <div className="grid place-items-center h-screen">
       <SigninButton/>
