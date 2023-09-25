c2d2b4f62c8c0ed153c9a960333f01fdfa48f074
Sachin <vsachin220@gmail.com>
2023-09-25 11:57:37 +0530

    fix: do not use env variable

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 const Chat: React.FC<Props> = ({ chat }) => {
   return (
-    <Link href={`${process.env.NEXTAUTH_URL}/chat/${chat.id}`}>
+    <Link href={`https://chatcoffee.netlify.app/chat/${chat.id}`}>
       <div className="cursor-pointer bg-slate-200 dark:bg-slate-900 px-1 pt-2 h-16">
         <h1 className="font-semibold text-xl">{chat.name}</h1>
       </div>
