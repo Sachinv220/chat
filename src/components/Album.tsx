0c4b7cf2e4435c6d47a85834a2b09305063425d9
Sachin <vsachin220@gmail.com>
2023-11-02 19:07:05 +0530

    fix: layout issues in for 3 images

--- src/components/Album.tsx
+++ src/components/Album.tsx
@@ -12,12 +12,18 @@ const Album: React.FC<Props> = ({ images }) => {
   const length = images.length > 4 ? 4 : images.length;
   let className = "";
   if (length !== 1) className = `grid grid-rows-2 grid-cols-2`;
+
+  const layoutClass = (idx: number): string => {
+    if (length === 3) return `object-cover ${idx >= 2 && "col-span-2"}`;
+    return "object-cover";
+  };
+
   return (
     <Avatar className={`mt-1 ${className}`}>
       {images.map((image, index) => (
         <Image
           key={index}
-          className={`object-cover ${index >= 2 ? "col-span-2" : ""}`}
+          className={layoutClass(index)}
           src={image}
           alt="Picture"
           width={100}
