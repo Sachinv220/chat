6b64b63cb622cd362d70daf72c53717fc60d12f3
Sachin <vsachin220@gmail.com>
2024-02-05 21:22:58 +0530

    remove checks for duplicate dm

--- src/components/CreateDM.tsx
+++ src/components/CreateDM.tsx
@@ -30,8 +30,6 @@ const CreateDM: React.FC<Props> = ({ userEmail, onFailure, onSuccess }) => {
     setOpen(false);
     if (res === Response.SERVER_ERROR) {
       onFailure("The user your trying to find does not exist");
-    } else if (res === Response.CONFILCT) {
-      onFailure("The DM already exists");
     } else {
       onSuccess(res);
     }
